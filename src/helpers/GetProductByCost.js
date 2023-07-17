06fadc89af4b17c4c723f72d0c1ee96b8516e972
felipemat6767 <felipe_6767@hotmail.es>
2023-07-16 23:40:23 -0400

    16-07

---
+++ src/helpers/GetProductByCost.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ProviderContext } from '../Provider/Provider';
+
+const GetProductByCategory = ({category}) => {
+    const { db } = useContext(ProviderContext);
+
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GetProductByCategory

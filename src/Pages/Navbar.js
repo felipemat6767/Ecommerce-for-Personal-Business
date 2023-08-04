721c5f35b00ce892d5da34aaad52932413358aab
felipemat6767 <felipe_6767@hotmail.es>
2023-08-03 23:39:09 -0400

    03-08

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -2,20 +2,17 @@ import React, { useState, useContext } from 'react'
 import { NavLink, Navigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import ShoppingCartSection from '../components/ShoppingCart';
 import { ProviderContext } from '../Provider/Provider';
 import GetSearchElem from '../helpers/getSearchElem';
 
 const Navbar = () => {
     const [active, setActive] = useState(false)
-    const { shoppingCart, searchElem } = useContext(ProviderContext);
+    const { shoppingCart} = useContext(ProviderContext);
     const openMenu = () => {
         setActive(!active)
     }
-    const SentSearchElem = () => {
-       
-    }
+
     return (
         <nav className='NavLinks'>
             <span>
@@ -28,7 +25,7 @@ const Navbar = () => {
                 <NavLink to="/Pictures">Pictures</NavLink>
                 <NavLink >More ...</NavLink>
                 <GetSearchElem />
-                <a href = "/SearchedElem" onClick={SentSearchElem}><FontAwesomeIcon icon={faSearch}/></a>
+                
             </div>
          
             <FontAwesomeIcon icon={faCartShopping} onClick={openMenu} className='navIcons' />
--- src/helpers/getSearchElem.js
+++ src/helpers/getSearchElem.js
@@ -3,6 +3,9 @@ import { ProviderContext } from '../Provider/Provider';
 import { useLocation, useNavigate } from 'react-router-dom'
 import queryString from "query-string"
 import useForm from '../hooks/useForm';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Producto from '../components/Producto';
 
 const GetSearchElem = () => {
     const { setSearchElem } = useContext(ProviderContext);
@@ -15,28 +18,31 @@ const GetSearchElem = () => {
     })
 
     const { searchText } = formvalues
-    const heroesfiltered =  setSearchElem(q)
-  
+    const productsFiltered = setSearchElem(q)
+
     const handlesearch = (e) => {
         e.preventDefault()
         navigate(`?q=${searchText}`)
     }
+
+    console.log(productsFiltered)
+    console.log(searchText)
     return (
         <>
             <form onSubmit={handlesearch}>
                 <input
                     type="text"
-                    placeholder='Buscar un heroe'
+                    placeholder='Buscar un Producto'
                     className='form-control'
                     name="searchText"
                     value={searchText}
                     onChange={handleinputchange}
                 />
 
-                <button type="submit" className='btn btn-outline-primary mt-1'>
-                    Search ...
-                </button>
+                <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
             </form>
+
+           
         </>
     )
 }

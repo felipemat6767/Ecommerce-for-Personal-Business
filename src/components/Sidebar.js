47eb5341ba0486cf2f8fef3f25851d20b5a7c9f6
felipemat6767 <felipe_6767@hotmail.es>
2023-08-23 19:04:29 -0400

    23-08

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,43 @@
 import React, { useContext } from 'react'
 import { ProviderContext } from '../Provider/Provider';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { useState } from "react";
 const Sidebar = () => {
-  const { setCategory} = useContext(ProviderContext);
-  return (
+  const [view, setView] = useState(false);
+  const { setCategory } = useContext(ProviderContext);
+
+  const setRange = () => {
+      setView(!view)
+  }
+    return (
     <>
       <ul className='Sidebar'>
         <li>Browse</li>
-        <li onClick={() => {setCategory("")}}>All</li>
-        <li onClick={() => {setCategory("Men")}}>Men</li>
-        <li onClick={() => {setCategory("Women")}}>Women</li>
-        <li onClick={() => {setCategory("Jewe")}}>.925 Jewelry</li>
+        <li onClick={() => { setCategory("") }}>All</li>
+        <li onClick={() => { setCategory("Men") }}>Men</li>
+        <li onClick={() => { setCategory("Women") }}>Women</li>
+        <li onClick={() => { setCategory("Jewe") }}>.925 Jewelry</li>
+        <div className='range'>
+          <span>Range </span>
+          <div className='range-icon' >
+            <FontAwesomeIcon icon={faTrash} onClick={setRange} />
+          </div>
+          {view && <li>
+            <div className='range-input'>
+              <div className='range-input-cont'>
+                <label>Min Price</label>
+                <input></input>
+              </div>
+              <div className='range-input-cont'>
+                <label>Min Price</label>
+                <input></input>
+              </div>
+            </div>
+          </li>}
+          
+        </div>
+
       </ul>
     </>
   )
